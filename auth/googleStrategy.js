2bef6fbffc39726448acfc2f4b0f3d9c17f68670
Krati Agarwal <kratiag190@gmail.com>
2025-05-12 22:44:59 +0530

    auth

--- auth/googleStrategy.js
+++ auth/googleStrategy.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'https://crm-backend-d0ny.onrender.com/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   // I can store profile info in DB here
   done(null, profile);
